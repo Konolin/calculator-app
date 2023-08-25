36257a11e4eae1dcff0ce1156cf8a8ce6603d4d0
Konolin <mandru.catalin03@gmail.com>
2023-08-25 12:59:31 +0300

    Init

--- src/Calculator.java
+++ src/Calculator.java
@@ -1,4 +1,8 @@
-public class Main {
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+
+public class Calculator {
     public static void main(String[] args) {
         System.out.println("Hello world!");
     }
